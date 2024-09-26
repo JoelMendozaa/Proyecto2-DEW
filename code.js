fd1c3b73f83d74c233dbd858c5ecfd91fbf07a12
Joel <yowismr@gmail.com>
2024-09-26 12:46:47 +0100

    Update binter iberia vueling y code

--- code.js
+++ code.js
@@ -1,11 +1,39 @@
 function Avion (name, rows, column, price){     // Objeto avión
     this.name = name;
-    this.rows = rows;
+    this.rows = rows;           // Constructor
     this.column = column;
     this.priceBase = price;
 }
 
-// Se crea los objetos con sus respectivos nombres de las aerolineas.
+// Se crea los objetos con sus respectivos nombres de las aerolineas y constructor.
 const binter = new Avion("Binter", 30, 6, 120);
 const iberiaExpress = new Avion ("Iberia", 40, 6, 100);
 const vueling = new Avion ("Vueling", 15, 4, 140);
+
+
+function Asientos (Avion){
+
+
+}
+
+
+
+
+
+/*function Cliente (){
+    var clase = prompt("¿En qué tipo de clase quieres viajar?");
+    var asiento = prompt("Elige el asiento");
+    var asientoRandom = Math.random();
+
+    while (clase != "Business" || clase != "Turista" || clase != "LowCost"){
+        alert("Esa clase no existe. Por favor, elija su clase")
+
+        if (clase == "Busines"){
+            asiento
+        } else if (clase == "Turista"){
+            asientoRandom
+        }else{
+            asientoRandom
+        }
+    }
+}*/
