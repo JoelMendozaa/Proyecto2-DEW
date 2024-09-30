92952949dd4f6d9eccf96c88a97c437870a462c9
Joel <yowismr@gmail.com>
2024-09-30 09:52:34 +0100

    Update code.js style.css

--- code.js
+++ code.js
@@ -3,6 +3,7 @@ function Avion (name, rows, column, price){     // Objeto avión
     this.rows = rows;           // Constructor
     this.column = column;
     this.priceBase = price;
+    this.color = [];
 }
 
 // Se crea los objetos con sus respectivos nombres de las aerolineas y constructor.
@@ -12,12 +13,14 @@ const vueling = new Avion ("Vueling", 15, 4, 140);
 
 
 function asientos (avion){
+
     document.write(`<table>`);
     for (let fila = 0; fila < avion.rows; fila ++) {        // Generación asientos
-
+        
         let claseFila = '';
         if (fila <= 3){
             claseFila = 'fila-naranja';     // Color a los asientos de primera Clase
+            
         } else if (fila > 3 && fila <= 9){
             claseFila = 'fila-azul';        // Color a los asientos de clase Turista
         }
@@ -49,31 +52,64 @@ function bienvenida(){
 }
 
 function eleccion(){
-    var clase = prompt("¿En qué clase le gustaría viajar? Clase Alta, Turista o Económica");
 
-    while (clase != "Alta" && clase != "Turista" && clase != "Económica"){
+    var cantidadTicket = parseInt (prompt("¿Cuántos billetes desea comprar?"));
+    while (cantidadTicket <= 0){
+        alert ("Adiós");
+        return;
+    }
+
+    var clase = prompt("¿En qué clase le gustaría viajar? Clase Business, Turista o Económica");
+
+    while (clase != "Business" && clase != "Turista" && clase != "Económica"){
         alert ("Esa clase no existe");          // Bucle While para que siempre que no se escriba una de las opciones salte un error
         clase = prompt("¿En qué clase le gustaría viajar?");
     } 
-    
-    if (clase == "Alta"){
-        var maleta = prompt ("¿Tiene alguna maleta que quiera abordar?");
-        while (maleta != "Si" && maleta != "No"){
-            alert ("Lo lamento, debes responder con un Si o con un No");
-            maleta = prompt ("¿Tiene alguna maleta que quiera abordar?");
-        }
-        if (maleta == "Si"){
-            var peso = parseInt (prompt ("¿De cuántos kg es? [5kg, 10kg o 25kg]"));
-            while(peso != 5 && peso != 10 && peso != 25){
-                alert("Peso incorrecto");
-                peso = parseInt (prompt ("¿De cuántos kg es? [5kg, 10kg o 25kg]"));
+
+    if (cantidadTicket > 0){
+        for (var i = 0; i < cantidadTicket; i++){
+
+            
+            if (clase == "Business"){
+                var maleta = prompt ("¿Tiene alguna maleta que quiera abordar?");
+                while (maleta != "Si" && maleta != "No"){
+                    alert ("Lo lamento, debes responder con un Si o con un No");
+                    maleta = prompt ("¿Tiene alguna maleta que quiera abordar?");
+                }
+                if (maleta == "Si"){
+                    var peso = parseInt (prompt ("¿De cuántos kg es? [5kg, 10kg o 25kg]"));
+                    while(peso != 5 && peso != 10 && peso != 25){
+                        alert("Peso incorrecto");
+                        peso = parseInt (prompt ("¿De cuántos kg es? [5kg, 10kg o 25kg]"));
+                    }
+                    console.log ("Maleta abordada");
+                } else if (maleta == "No"){
+                    console.log ("Sin maleta");
+                }            
             }
-            console.log ("Maleta abordada");
-        } else if (maleta == "No"){
-            console.log ("Sin maleta");
-        } 
-        
+        }
     }
+     /*var comida = prompt("¿Desea comer algo?");
+    while (comida != "Si" || comida != "No"){
+        alert("Eso no es una respuesta, por favor, introduzca si o no");
+        comida = prompt ("¿Desea comer algo?");
+    }
+    if (comida == "Si"){
+        var menu = prompt ("¿Eres celiaco?");
+        while (menu != "Si" && menu != "No"){
+            alert ("La respuesta debe ser si o no");
+            menu = prompt ("¿Eres celiaco?")
+        }
+        if (menu == "Si"){
+            console.log("Es celiaco");
+        } else {
+            console.log ("No es celiaco");
+        }
+        
+    } else if (comida == "No"){
+        alert ("De acuerdo");
+    }*/
+
 }
 
 function eleccionAsientos(){
@@ -89,7 +125,9 @@ function eleccionAsientos(){
         asientoFila = parseInt(prompt ("¿En qué columna desea sentarse?"));
     }
 
-    var asientoId = `asiento - ${asientoFila} - ${asientoColumna}`;
+   
+
+    /*var asientoId = `asiento - ${asientoFila} - ${asientoColumna}`;
     var asientoSeleccionado = document.getElementById(asientoId);       // Variable creada con el id del asiento que se ha seleccionado previamente 
     if (asientoSeleccionado) {
         asientoSeleccionado.style.backgroundColor = "green";  // Cambiar el color del asiento a verde
@@ -98,6 +136,5 @@ function eleccionAsientos(){
     } else {
         alert("No se pudo seleccionar el asiento.");
         console.log("No se puede dibujar")
-    }
+    }*/
 }
-
